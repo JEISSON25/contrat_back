0be2dc44cd2b3b4388e736bf1af2bc3dc144aa39
INGJEISSON <jeisim18@gmail.com>
2025-04-02 07:36:41 -0500

    fix

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/generate-presigned-url', async (req, res) => {
 });
 
 // Inicia el servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
